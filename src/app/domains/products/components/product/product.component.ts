9ffb1d1c80f9ecfa105f7b49c9d33088e04360ab
juanroam <juanc.rodrigueza@hotmail.com>
2025-03-30 12:20:40 -0500

    New component and page for products
    - Domain driven structure
    - Route config for List of Products

---
+++ src/app/domains/products/components/product/product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  img = 'https://picsum.photos/640/640?r=' + Math.random();
+}
