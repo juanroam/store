ed90209ee2f805a89498e4d1e1d9b61f2387fb85
juanroam <juanc.rodrigueza@hotmail.com>
2025-09-28 11:16:48 -0500

    Product Detail Page base and routing

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -2,10 +2,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '@shared/models/product.model';
 import { CommonModule } from '@angular/common';
 import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-product',
-  imports: [CommonModule, TimeAgoPipe],
+  imports: [CommonModule, TimeAgoPipe, RouterLink],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
