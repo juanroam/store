be38d39a056e919845b020c45588e829a4b47bde
juanroam <juanc.rodrigueza@hotmail.com>
2025-09-28 19:24:54 -0500

    Chunks loading optimization with preloading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { PreloadAllModules, provideRouter, withComponentInputBinding, withPreloading } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http'
 
 import { routes } from './app.routes';
@@ -7,8 +7,11 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    /** Configure app to receive parameters as inputs in pages */
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(
+      routes,
+      withComponentInputBinding(), //Configure app to receive parameters as inputs in pages
+      withPreloading(PreloadAllModules), //Chunks loading optimization with preloading
+    ),
     provideHttpClient(),
   ],
 };
